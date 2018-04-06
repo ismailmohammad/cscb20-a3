a67e885a0a35f0f7aed96cfcf50a3b8a6acef17a
ismail.mohammad.n <ismail.mohammad.n@gmail.com>
2018-04-06 05:31:39 -0400

    sql error typo fix debug debug debug

--- viewremarks.php
+++ viewremarks.php
@@ -13,7 +13,7 @@ if(!$conn)
 {
   header("location:error.html");
 }
-$sqlselect = 'select a.status, a.request b.firstname, b.lastname, b.username, c.name, a.request_id from remarks a join users b on a.submitted_id=b.id join assignments c on c.id=a.assignment_id' ? $_SESSION['type'] == 3 : $sqlselect = 'select a.status, a.request, b.firstname, b.lastname, b.username, c.name, a.request_id from remarks a join users b on a.submitted_id=b.id where directed_id='.$_SESSION['userid'].' join assignments c on c.id=a.assignment_id';
+$sqlselect = 'select a.status, a.request, b.firstname, b.lastname, b.username, c.name, a.request_id from remarks a join users b on a.submitted_id=b.id join assignments c on c.id=a.assignment_id' ? $_SESSION['type'] == 3 : $sqlselect = 'select a.status, a.request, b.firstname, b.lastname, b.username, c.name, a.request_id from remarks a join users b on a.submitted_id=b.id where directed_id='.$_SESSION['userid'].' join assignments c on c.id=a.assignment_id';
 
 $retval = mysqli_query($conn, $sqlselect);
 if(!$retval)
