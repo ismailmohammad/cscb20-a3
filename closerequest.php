4b7dc828024e2330be8b28dfeb52b3283bf4c505
ismail.mohammad.n <ismail.mohammad.n@gmail.com>
2018-04-06 05:42:21 -0400

    closerequest.php debug

--- closerequest.php
+++ closerequest.php
@@ -14,7 +14,7 @@ $dbname = 'cscb20w18_sohanisa';
 $conn = mysqli_connect($dbhost, $dbuser, $dbpass, $dbname, 3306);
 if(!$conn)
 {
-  header("location:error.html");
+	header("location:error.html");
 }
 
 $sqlselect = 'update remarks set status=0 where remarks.rid='.$rid;
@@ -22,9 +22,9 @@ $sqlselect = 'update remarks set status=0 where remarks.rid='.$rid;
 $retval = mysqli_query($conn, $sqlselect);
 if(!$retval)
 {
-	header("location:error.html");
+// 	header("location:error.html");
+		echo mysqli_error($conn);
 }
-
-echo "<script type='text/javascript'>alert('Remark Request Closed Successfully!'); window.location.href='https://mathlab.utsc.utoronto.ca/cscb20/sohanisa/viewremarks.php'</script>";
+//echo "<script type='text/javascript'>alert('Remark Request Closed Successfully!'); window.location.href='https://mathlab.utsc.utoronto.ca/cscb20/sohanisa/viewremarks.php'</script>";
 mysqli_close($conn);
 ?>
