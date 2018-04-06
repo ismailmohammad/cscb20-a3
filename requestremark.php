cd95f8dac9256da8d43a8026d42a22037d005d6f
ismail.mohammad.n <ismail.mohammad.n@gmail.com>
2018-04-06 04:13:33 -0400

    add already submitted check - hopefully final of remarkform and requestremark .php

--- requestremark.php
+++ requestremark.php
@@ -17,6 +17,25 @@ if(!$conn)
 }
 
 $sid = $_SESSION['userid'];
+
+$sqlselect = 'select assignment_id from remarks where submitted_id='.$sid;
+
+$retval = mysqli_query($conn, $sqlselect);
+if(!$retval)
+{
+	header("location:error.html");
+}
+
+while($row = mysqli_fetch_array($retval, MYSQLI_NUM))
+{
+	if ($row[0] == $assignmentid) {
+    echo "<script type='text/javascript'>alert('Remark Already Submitted for this assignment'); clearRemarkField();'</script>";
+    die("Already Submitted");
+  }
+}
+
+mysqli_free_result($retval);
+
 $sql = "INSERT INTO remarks ". "(submitted_id, directed_id, status, request, assignment_id) ".
 "VALUES ('$sid', '$tid', '0', '$request', '$assignmentid')";
 $retval = mysqli_query($conn, $sql);
