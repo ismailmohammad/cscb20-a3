7d4ceccefd4a0f23cb5938b01c8a6632b9877f15
ismail.mohammad.n <ismail.mohammad.n@gmail.com>
2018-04-06 01:59:27 -0400

    allgrades test

--- allgrades.php
+++ allgrades.php
@@ -14,7 +14,7 @@ if(!$conn)
   header("location:error.html");
 }
 
-$sqlselect = 'select c.firstname, c.lastname, c.id from grades a join users c where a.student_id=c.id order by a.student_id desc'
+$sqlselect = 'select c.firstname, c.lastname, c.id from grades a join users c where a.student_id=c.id order by a.student_id desc';
 $retval = mysqli_query($conn, $sqlselect);
 if(!$retval)
 {
