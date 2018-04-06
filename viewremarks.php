55eb3a140490d7041ba557e6ef97b4e432bdfbdc
ismail.mohammad.n <ismail.mohammad.n@gmail.com>
2018-04-06 05:23:57 -0400

    commentout header

--- viewremarks.php
+++ viewremarks.php
@@ -16,8 +16,8 @@ if(!$conn)
 $sqlselect = 'select a.status, a.request b.firstname, b.lastname, b.username, c.name, a.request_id from remarks a join users b on a.submitted_id=b.id join assignments c on c.id=a.assignment_id' ? $_SESSION['type'] == 3 : 'select a.status, a.request, b.firstname, b.lastname, b.username, c.name, a.request_id from remarks a join users b on a.submitted_id=b.id where directed_id='.$_SESSION['userid'].' join assignments c on c.id=a.assignment_id';
 
 $retval = mysqli_query($conn, $sqlselect);
-if(!$retval)
-  header("location:error.html");
+//if(!$retval)
+  //header("location:error.html");
 
 $statuses = array();
 $requests = array();
